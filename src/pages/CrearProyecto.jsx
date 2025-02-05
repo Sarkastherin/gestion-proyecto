b7a92c72fd622076e13affcb8d38853a54d6696f
sarkastherin <kathejudith@gmail.com>
2025-02-05 06:46:04 -0300

    created componente BoxComponent

--- src/pages/CrearProyecto.jsx
+++ src/pages/CrearProyecto.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import { BoxComponent } from "../components/BoxComponent";
 import { Label, Input, Textarea, InputGroup } from "../components/Inputs";
 import { useForm } from "react-hook-form";
 import { ButtonIcons, Button } from "../components/Buttons";
@@ -10,48 +11,56 @@ function CrearProyecto() {
   return (
     <>
       <Header text={"Creando proyecto"} />
-
-      <form className="" onSubmit={handleSubmit(onSubmit, onError)}>
-        <div className="max-w-5xl mx-auto mt-10 px-4">
-          <div className="md:columns-2">
-            <div className="w-full">
-              <Label label={"Nombre del proyecto"} htmlFor={"nombreProyecto"} />
-              <Input
+      <BoxComponent title={"Datos de la Oportunidad"}>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <div className="">
+            <div className="md:columns-2">
+              <div className="w-full">
+                <Label
+                  label={"Nombre del proyecto"}
+                  htmlFor={"nombreProyecto"}
+                />
+                <Input
+                  className="mt-1"
+                  {...register("nombreProyecto")}
+                  placeholder="Nombre del proyecto"
+                />
+              </div>
+              <div className="w-full">
+                <Label label={"Cliente"} htmlFor={"cliente"} />
+                <InputGroup
+                  {...register("cliente")}
+                  placeholder="Buscar CLiente"
+                >
+                  <ButtonIcons
+                    variant="secondaryOutline"
+                    type={"button"}
+                    icon={<BuildingOffice2Icon width={"16px"} />}
+                  />
+                </InputGroup>
+              </div>
+            </div>
+            <div className="w-full mt-3">
+              <Label label={"Alcance"} htmlFor={"alcance"} />
+              <Textarea
                 className="mt-1"
-                {...register("nombreProyecto")}
-                placeholder="Nombre del proyecto"
+                {...register("alcance")}
+                placeholder="Alcance"
               />
             </div>
-            <div className="w-full">
-              <Label label={"Cliente"} htmlFor={"cliente"} />
-              <InputGroup {...register("cliente")} placeholder="Buscar CLiente">
-                <ButtonIcons
-                  variant="secondaryOutline"
-                  icon={<BuildingOffice2Icon width={"16px"} />}
-                />
-              </InputGroup>
+          </div>
+          <div className="absolute bottom-0 bg-neutral-300 w-full">
+            <div className="flex gap-4 py-4 justify-end pr-12">
+              <Button type="button" variant="secondary" className="">
+                Cerrar
+              </Button>
+              <Button type="submit" onSubmit={handleSubmit(onSubmit, onError)}>
+                Guardar
+              </Button>
             </div>
           </div>
-          <div className="w-full mt-3">
-            <Label label={"Alcance"} htmlFor={"alcance"} />
-            <Textarea
-              className="mt-1"
-              {...register("alcance")}
-              placeholder="Alcance"
-            />
-          </div>
-        </div>
-        <div className="absolute bottom-0 bg-neutral-300 w-full">
-          <div className="flex gap-4 py-4 justify-end pr-12">
-            <Button type="button" variant="secondary" className="">
-              Cerrar
-            </Button>
-            <Button type="submit" onSubmit={handleSubmit(onSubmit, onError)}>
-              Guardar
-            </Button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </BoxComponent>
     </>
   );
 }
