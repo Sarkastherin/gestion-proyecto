e60d01f1b16be1289c5f9020817d1b1f9f72afd2
Katherin Guerrero <kathejudith@gmail.com>
2025-04-06 19:57:30 -0300

    comenzando duplicacion de cotizacion

--- src/components/Generals/Badge.jsx
+++ src/components/Generals/Badge.jsx
@@ -1,9 +1,10 @@
-export default function Badge({ text, variant }) {
+export default function Badge({ variant, className, children }) {
   const baseClasses = "rounded-full px-2.5 py-0.5 text-sm whitespace-nowrap";
   const variants = {
     purple: "bg-purple-100 text-purple-700",
     yellow: "bg-yellow-100 text-yellow-700",
     pink: "bg-pink-100 text-pink-700",
+    blue: "bg-blue-100 text-blue-700",
   };
-  return <span className={`${baseClasses} ${variants[variant]}`}>{text}</span>;
+  return <span className={`${baseClasses} ${variants[variant]} ${className}`}>{children}</span>;
 }
--- src/components/ModalCotizaciones.jsx
+++ src/components/ModalCotizaciones.jsx
@@ -4,51 +4,36 @@ import { UserGroupIcon, CurrencyDollarIcon } from "@heroicons/react/16/solid";
 import { useState, useEffect } from "react";
 import { useModal } from "../context/ModalContext";
 import { useFormContext, useForm } from "react-hook-form";
+import { useOportunidad } from "../context/Oportunidades/OportunidadContext";
+import { useCotizacion } from "../context/Cotizaciones/CotizacionesContext";
+import Badge from "./Generals/Badge";
 export const ModalCotizaciones = () => {
-  const [search, setSearch] = useState("");
-  const [selectCotizacion, setSelectCotizacion] = useState({});
+  const { oportunidades } = useOportunidad();
+  const { cotizaciones, getCotizaciones } = useCotizacion();
   const { handleModalShow, handleModalClose } = useModal();
   const [filteredData, setFilteredData] = useState([]);
+  const [state, setState] = useState({
+    cotizacion: [],
+  });
   const {
     register,
     formState: { errors },
     setValue,
   } = useFormContext();
-  /* ;
-  
-  const [clientes, setClientes] = useState([]);
-  
-  
-  
-  const getClientes = async () => {
-    try {
-      const response = await fetch(
-        "https://fakerapi.it/api/v2/companies?_quantity=20"
-      );
-      const { status, code, data } = await response.json();
-      setClientes(data);
-      setFilteredData(data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
   useEffect(() => {
-    getClientes();
+    getCotizaciones();
   }, []);
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      const result = clientes.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase())
-      );
-      setFilteredData(result);
-    }, 300); // Agrega un debounce de 300ms
-    return () => clearTimeout(timeout);
-  }, [search]);
-  useEffect(() => {
-    if (selectCotizacion?.name) {
-      setValue("cliente", selectCotizacion.name, { shouldDirty: true });
+    if (cotizaciones.length > 0) {
+      cotizaciones.forEach((item) => {
+        item.nombre_oportunidad = oportunidades.find(
+          (elem) => elem.id === item.id_oportunidad
+        ).nombre;
+        return item;
+      });
     }
-  }, [selectCotizacion, setValue]); */
+    setFilteredData(cotizaciones);
+  }, [oportunidades, cotizaciones]);
   return (
     <>
       <Modal
@@ -56,27 +41,42 @@ export const ModalCotizaciones = () => {
         title={"Buscar Cotización"}
         variant="primary"
         icon={<CurrencyDollarIcon className="w-6" />}
+        width="w-3xl"
       >
         <div className="mt-4">
-          <p className="my-1 text-sm text-gray-700">Seleccione un cotización.</p>
+          <p className="my-1 text-sm text-gray-700">
+            Seleccione un cotización.
+          </p>
           <Input
-          label="Buscar Cotización"
-          no_label
+            label="Buscar Cotización"
+            no_label
             type="search"
             placeholder="Buscar Cotización"
             onInput={(e) => setSearch(e.target.value)}
           />
           <ul className="mt-2 max-h-[300px] overflow-y-auto">
-            {filteredData.map((client) => (
+            {filteredData.map((cotizacion) => (
               <li
                 className="mt-2 text-sm text-gray-600 rounded border border-gray-300 px-4 py-1 cursor-pointer hover:bg-indigo-100"
-                key={client.id}
+                key={cotizacion.id}
                 onClick={() => {
-                  setSelectCotizacion(client);
+                  //setSelectCotizacion(cotizacion);
                   handleModalClose();
                 }}
               >
-                <p>{client.name}</p>
+                <span className="flex">
+                  <span className="basis-2/3">{cotizacion.nombre_oportunidad}</span>
+                  <span className="flex basis-1/3">
+                    <Badge variant="yellow" className={'w-2/3'}>
+                    Monto: 
+                      {cotizacion.total_monto.toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      })}
+                    </Badge>
+                    <Badge variant="blue" className={'w-1/3'}>{cotizacion.status}</Badge>
+                  </span>
+                </span>
               </li>
             ))}
           </ul>
