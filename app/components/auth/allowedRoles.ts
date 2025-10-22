fdd82ec3688aac742ca453129c3168b814c6c51a
Katherin Guerrero <kathejudith@gmail.com>
2025-10-22 14:51:45 -0300

    agregando roles

--- app/components/auth/allowedRoles.ts
+++ app/components/auth/allowedRoles.ts
@@ -5,5 +5,5 @@ export const ALLOWED_NEW_OPPORTUNITY: Roles[] = ["administrador", "dueño", "coo
 export const ALLOWED_OPPORTUNITIES: Roles[] = ["administrador", "dueño", "coordinador"];
 export const ALLOWED_REPORTS_EMPLOYEES: Roles[] = ["administrador", "dueño"];
 export const ALLOWED_SETTINGS: Roles[] = ["administrador", "dueño", "coordinador"];
-export const ALLOWED_PROJECTS: Roles[] = ["administrador", "dueño"];
+export const ALLOWED_PROJECTS: Roles[] = ["administrador", "dueño", "supervisor", "coordinador"];
 export const ALLOWED_RRHH: Roles[] = ["administrador", "dueño"];
